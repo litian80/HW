af217302866176d743f6e910b42cd040dc112eaa
litia <litia@DESKTOP-U85740E>
2017-07-16 23:07:45 +1200

    Add project files.

---
+++ HW/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "this is my <b>default</b> action...";
+        }
+
+
+        public string Welcome()
+        {
+            return "This is the welcome action method...";
+        }
+    }
+}
